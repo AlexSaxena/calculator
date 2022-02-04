c3fece2d1f862060c219670205b38b99e9386db1
AlexSaxena <saxenaalex@gmail.com>
2022-02-04 15:16:39 +0100

    Adds test listeners for 1,2,C + btnloop psudocode

--- script.js
+++ script.js
@@ -19,3 +19,28 @@ let btnThree = document.getElementById("btn-three");
 let btnTwo = document.getElementById("btn-two");
 let btnOne = document.getElementById("btn-one");
 let btnZero = document.getElementById("btn-zero");
+let calcScreen = document.getElementById("calc-screen");
+let numpad = document.getElementsByClassName("numpad");
+
+let btnTest = document.getElementById("btn-test");
+let gridMain = document.getElementsByClassName("grid-main");
+let nums = document.getElementsByClassName("nums");
+
+// Loopa igenom alla knappar för att hitta value, alla knappar har samma class
+// skapa en let buttons -> med en loop button length.
+// inuti loop, ta button[index] och med funktion
+// skapa en ONCLICk där button[index].value skickas till +=calcScreen.value
+
+function getBtnValue(current) {
+  calcScreen.value += current.value;
+}
+
+btnOne.addEventListener("click", () => {
+  calcScreen.value += 1;
+});
+btnTwo.addEventListener("click", () => {
+  calcScreen.value += btnTwo.value;
+});
+btnClear.addEventListener("click", () => {
+  calcScreen.value = null;
+});
